81adde34070e5394d75573ff1e1e099aba50f169
Himanshu Ranjan <ranjanhimanshu272005@gmail.com>
2026-06-03 12:53:50 +0530

    Add files via upload

---
+++ JobAppApplication.java
@@ -0,0 +1,15 @@
+package com.telusko.JobApp;
+//.\mvnw spring-boot:run
+import org.springframework.boot.SpringApplication;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAppApplication.class, args);
+	}
+
+}
